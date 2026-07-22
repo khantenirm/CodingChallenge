31635988c317ccd9202bf5e65e18b88a29e86077
Khantenir <khantenirm@gmail.com>
2026-07-22 10:00:46 +0600

    Argentina bye bye

--- src/main/java/wc/Argentina.java
+++ src/main/java/wc/Argentina.java
@@ -2,6 +2,6 @@ package wc;
 
 public class Argentina {
     public static void main(String[] args) {
-        System.out.println("Hello Argentina!!!");
+        System.out.println("Bye Argentina!!!");
     }
 }
